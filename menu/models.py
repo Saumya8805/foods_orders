a1bd1ec115f56b1f32ceb64ba68d8fe017f232de
Saumya <Smishra8805@gmail.com>
2026-09-18 20:38:29 +0530

    refactor: Clean up unused code and improve URL routing in menu app

--- menu/models.py
+++ menu/models.py
@@ -18,18 +18,8 @@ class FoodItem(models.Model):
 
 
 
-    # menu/models.py
-from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
+   
 
-class Food(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='foods/')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="foods")
 
--- menu/urls.py
+++ menu/urls.py
@@ -1,17 +1,9 @@
 from django.urls import path
 from . import views
-from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.menu_list, name='menu_list'),
-
-
-
-
-
-
-    path('category/<int:id>/', views.category_detail, name='category_detail'),
-
-
+   
 ]
